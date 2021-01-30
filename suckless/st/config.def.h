831f045e2e6f2e792c0cd00affa8a6f7a70b34df
Muhammad Mourad <muhammadmourad1995@gmail.com>
2021-01-30 16:31:39 +0200

    Fix scrollbar

--- suckless/st/config.def.h
+++ suckless/st/config.def.h
@@ -199,8 +199,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_j,           kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_k,           kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_j,           kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_k,           kscrolldown,    {.i = -1} },
 };
 
 /*
