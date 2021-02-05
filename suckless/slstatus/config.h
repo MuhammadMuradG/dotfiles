40f04eed794297762716316610f5b57daf91c113
Muhammad Mourad <muhammad.mourad1995@gmail.com>
2021-02-05 17:26:21 +0200

    Fix slstatus config file

--- suckless/slstatus/config.h
+++ suckless/slstatus/config.h
@@ -90,5 +90,5 @@ static const struct arg args[] = {
 	{ keymap,           " %s",              NULL },
 	{ separator,        SEPARATOR,         NULL },
 
-	{ datetime,         " %s",              "%d,%b %a %I:%M %p" },
+	{ datetime,         " %s",              "%d,%b %a %I:%M %p" },
 };
