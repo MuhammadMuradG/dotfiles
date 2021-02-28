7bc6ec0f05da84d9e6c832c62324bec88de974e6
Muhammad Mourad <muhammad.mourad1995@gmail.com>
2021-02-28 21:03:08 +0200

    Enhance font in st and dwm

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Noto Mono Nerd Font::pixelsize=11";
+static char *font = "Noto Mono Nerd Font:pixelsize=11:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
