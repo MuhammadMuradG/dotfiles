678843b63789064c5b53f6e8ffa1369680432041
Muhammad Mourad <muhammad.mourad1995@gmail.com>
2021-03-27 11:48:27 +0200

    Fix typoes and use zsh

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]        = { "st", "bash", NULL };
+static const char *termcmd[]        = { "st", NULL };
 static const char *browser[]        = { "firefox", "-p", NULL };
 static const char *lockcmd[]        = { "slock", NULL };
 static const char *volumemutecmd[]  = { "mixer", "vol",   "0", NULL };
