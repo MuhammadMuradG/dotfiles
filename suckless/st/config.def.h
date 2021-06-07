1c76db5330bed7f533eb733131daf6749d57764f
MuhammadMouradG <muhammad.mourad1995@gmail.com>
2021-06-07 04:41:20 +0200

    Increase font size in config tamplate suckless st emulator

--- suckless/st/config.def.h
+++ suckless/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "RobotoMono Nerd Font Mono:pixelsize=9:antialias=true:autohint=true";
+static char *font = "RobotoMono Nerd Font Mono:pixelsize=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
