ae4c24b78bd5955fa2afa9ea7a55384b0e47045e
MuhammadMouradG <muhammad.mourad1995@gmail.com>
2021-06-08 05:49:15 +0200

    Fix dmenu.

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "RobotoMono Nerd Font:pixelsize=10:style=bold" };
-static const char dmenufont[]       = "RobotoMono Nerd Font:pixelsize=9";
+static const char dmenufont[]       = "RobotoMono Nerd Font:pixelsize=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
