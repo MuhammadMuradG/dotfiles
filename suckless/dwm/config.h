193ab696f854152824468ade07e3ba8a1cbe9392
MuhammadMouradG <muhammad.mourad1995@gmail.com>
2023-09-16 19:22:33 +0200

    Set additional keyboard configs

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -76,7 +76,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_F11,     spawn,          {.v = volumemutecmd } },
 	{ MODKEY,                       XK_F9,      spawn,          {.v = volumedowncmd } },
 	{ MODKEY,                       XK_F10,     spawn,          {.v = volumeupcmd } },
-	{ Mod1Mask,                     XK_Shift_L, spawn,          SHCMD("setxkbmap -query | grep -q 'us' && setxkbmap ara || setxkbmap us") },
 	{ MODKEY,                       XK_F2,      spawn,          SHCMD("xrandr --output LVDS-1 --gamma 1.000000:0.790418:0.567936 --brightness 0.9") },
 	{ MODKEY,                       XK_F3,      spawn,          SHCMD("xrandr --output LVDS-1 --gamma 1:1:1 --brightness 1") },
 
