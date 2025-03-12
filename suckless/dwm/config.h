5f6457781030fa826102b1995f886a8efa744887
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2025-03-12 16:33:10 +0200

    Fix display settings.

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -81,8 +81,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_z,       changeunfocusopacity, {.f = +0.025} },
 	{ MODKEY|ShiftMask,             XK_m,       changeunfocusopacity, {.f = -0.025} },
 
-	{ MODKEY,                       XK_F5,      spawn,                SHCMD("xrandr --output default --gamma 1.0:0.8:0.6 --brightness 0.9") },
-	{ MODKEY,                       XK_F6,      spawn,                SHCMD("xrandr --output default --gamma 1:1:1 --brightness 1") },
+	{ MODKEY,                       XK_F5,      spawn,                SHCMD("xrandr --output eDP-1 --gamma 1.0:0.8:0.6 --brightness 0.9") },
+	{ MODKEY,                       XK_F6,      spawn,                SHCMD("xrandr --output eDP-1 --gamma 1:1:1 --brightness 1") },
 	{ MODKEY,                       XK_F12,     spawn,                SHCMD("~/dotfiles/scripts/extenddisplay.sh") },
 	{ MODKEY,                       XK_F3,      spawn,                {.v = volumeupcmd } },
 	{ MODKEY,                       XK_F2,      spawn,                {.v = volumedowncmd } },
