80788a0970293e913db830eec0e249cae24bfa66
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2025-03-13 02:00:32 +0200

    Fix secnod monitor configs.

--- scripts/extenddisplay.sh
+++ scripts/extenddisplay.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CHOICE=`echo -e "HDMI\nVGA" | dmenu -i -p "Choose Port to Extend Monitor: "`
+CHOICE=`echo -e "Extend\nDuplicate" | dmenu -i -p "Choose the action on second monitor: "`
 
-[ $CHOICE = "HDMI" ] `xrandr --output HDMI-1 --auto --right-of eDP-1 && feh --bg-scale ~/Pictures/Wallpapers/wallpaperflare.com_wallpaper.jpg &`
-[ $CHOICE = "VGA"  ] `xrandr --output VGA-1 --auto --right-of eDP-1 && feh --bg-scale ~/Pictures/Wallpapers/wallpaperflare.com_wallpaper.jpg &`
+[ $CHOICE = "Extend" ] `xrandr --output HDMI-2 --auto --right-of eDP-1 && feh --bg-scale ~/Pictures/Wallpapers/wallpaperflare.com_wallpaper.jpg &`
+[ $CHOICE = "Duplicate" ] `xrandr --output HDMI-2 --same-as eDP-1 && feh --bg-scale ~/Pictures/Wallpapers/wallpaperflare.com_wallpaper.jpg &`
