89b7bc3a3ebfd137e43dac7831160cc7ba1097da
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2025-03-16 23:50:14 +0200

    Fix display script.

--- scripts/extenddisplay.sh
+++ scripts/extenddisplay.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-CHOICE=`echo -e "Extend\nDuplicate" | dmenu -i -p "Choose the action on second monitor: "`
+CHOICE=$(echo -e "Extend\nDuplicate" | dmenu -i -p "Choose the action on second monitor: ")
 
-[ $CHOICE = "Extend" ] `xrandr --output HDMI-2 --auto --right-of eDP-1 && feh --bg-scale ~/Pictures/Wallpapers/wallpaperflare.com_wallpaper.jpg &`
-[ $CHOICE = "Duplicate" ] `xrandr --output HDMI-2 --same-as eDP-1 && feh --bg-scale ~/Pictures/Wallpapers/wallpaperflare.com_wallpaper.jpg &`
+if [ "$CHOICE" = "Extend" ]
+then
+    xrandr --output HDMI-2 --auto --right-of eDP-1
+elif [ "$CHOICE" = "Duplicate" ]
+then
+    xrandr --output HDMI-2 --same-as eDP-1
+fi
+
+feh --bg-scale ~/Pictures/Wallpapers/wallpaperflare.com_wallpaper.jpg &
