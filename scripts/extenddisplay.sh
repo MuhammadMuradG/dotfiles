d954faa06355c7891c19b28aa670da6c14c59dca
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2025-04-15 23:32:39 +0200

    scripts: Fix refresh and duplicate display script.

--- scripts/extenddisplay.sh
+++ scripts/extenddisplay.sh
@@ -5,12 +5,14 @@ CHOICE=$(echo -e "Refresh\nExtend\nDuplicate" | dmenu -i -p "Choose the action o
 if [ "$CHOICE" = "Refresh" ]
 then
     xrandr
+    xrandr --output eDP-1 --auto
+    xrandr --output HDMI-2 --auto
 elif [ "$CHOICE" = "Extend" ]
 then
     xrandr --output HDMI-2 --auto --right-of eDP-1
 elif [ "$CHOICE" = "Duplicate" ]
 then
-    xrandr --output HDMI-2 --same-as eDP-1
+    xrandr --output HDMI-2 --auto --scale-from 1920x1080 --same-as eDP-1
 fi
 
 feh --bg-scale ~/Pictures/Wallpapers/wallpaperflare.com_wallpaper.jpg &
